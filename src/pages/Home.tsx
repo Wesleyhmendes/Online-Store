53fbaaba103b807147d985180287d7ce58539d8f
Wesley Mendes <wesleymendes123321@gmail.com>
2023-08-17 16:46:32 -0300

    Requisito 4 - Liste as categorias de produtos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from 'react';
+import { getCategories } from '../services/api';
+
+type GetCategory = {
+  id: string,
+  name: string,
+};
+
 function Home() {
+  const [categories, setCategories] = useState<GetCategory[]>([]);
+
+  useEffect(() => {
+    const awaitApi = async () => {
+      const result = await getCategories();
+      setCategories(result);
+    };
+    awaitApi();
+  }, []);
+
   return (
-    <h1 className="title" data-testid="home-initial-message">
-      Digite algum termo de pesquisa ou escolha uma categoria.
-    </h1>
+    <div>
+      { categories.map((category) => (
+        <label
+          data-testid="category"
+          key={ category.id }
+          htmlFor=""
+        >
+          <input
+            type="radio"
+          />
+          { category.name }
+        </label>
+      )) }
+      <h1 className="title" data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h1>
+    </div>
   );
 }
 export default Home;
