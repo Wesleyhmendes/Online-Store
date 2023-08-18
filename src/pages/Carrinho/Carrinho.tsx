45f77449ad5a7d5577fbdfba3882b84521ba13f4
Wesley Mendes <wesleymendes123321@gmail.com>
2023-08-18 13:54:33 -0300

    [Grupo 44] - Requisito 10 Semi pronto

--- src/pages/Carrinho/Carrinho.tsx
+++ src/pages/Carrinho/Carrinho.tsx
@@ -1,9 +1,63 @@
+import { useState } from 'react';
+import Product from '../../components/Product/Product';
+import useLocalStorage from './hooks/useLocalStorage';
+
 function Carrinho() {
+  const [isThereProduct, setIsthereProduct] = useState(false);
+  const [qunatiyProduct, setQuantityProduct] = useState<number>(0);
+
+  const { readLocalStorage } = useLocalStorage('cartProducts');
+  const item = readLocalStorage;
+
+  if (item) setIsthereProduct(true);
+
+  const handleQuantityMore = () => {
+    setQuantityProduct((prev) => qunatiyProduct - 1);
+  };
+
+  const handleQuantityLess = () => {
+    setQuantityProduct((prev) => qunatiyProduct + 1);
+  };
+
   return (
-    <h1 data-testid="shopping-cart-empty-message">
-      Seu carrinho está vazio
-      <p>teste</p>
-    </h1>
+    <>
+      {
+        !isThereProduct && (
+          <h1 data-testid="shopping-cart-empty-message">
+            Seu carrinho está vazio
+          </h1>
+        )
+      }
+      {
+        isThereProduct && (
+          <section>
+            <label htmlFor="voltar">
+              <button>
+                <img
+                  src="src/utils/goBack.png"
+                  alt="voltar"
+                />
+              </button>
+              { ' ' }
+              Voltar
+            </label>
+            <h2>Carrinho de Compras</h2>
+            <div>
+              <button>❌</button>
+              <Product
+                id={ readLocalStorage.id }
+                thumbnail={ readLocalStorage.thumbnail }
+                title={ readLocalStorage.title }
+                price={ readLocalStorage.price }
+              />
+              <button onClick={ handleQuantityMore }>+</button>
+              <p>{ qunatiyProduct }</p>
+              <button onClick={ handleQuantityLess }>-</button>
+            </div>
+          </section>
+        )
+      }
+    </>
   );
 }
 export default Carrinho;
