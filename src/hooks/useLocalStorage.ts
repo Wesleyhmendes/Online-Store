020da14ea1733b5684c0ccf29950f29a46553f7f
Joao Gomes <joaoeduardogomes@gmail.com>
2023-08-18 13:48:56 -0300

    feat: useLocalStorage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,13 @@
+const useLocalStorage = () => {
+  const saveLocalStorage = (key: string, values: object[]) => {
+    localStorage.setItem(key, JSON.stringify(values));
+  };
+  const readLocalStorage = (key: string) => JSON.parse(
+    localStorage.getItem(key) as string,
+  );
+  const deleteLocalStorage = (key: string) => {
+    localStorage.removeItem(key);
+  };
+  return { saveLocalStorage, readLocalStorage, deleteLocalStorage };
+};
+export default useLocalStorage;
