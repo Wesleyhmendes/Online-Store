87a145da0f3f29313afbfe27c8ccc50e2b4f23de
Joao Gomes <joaoeduardogomes@gmail.com>
2023-08-18 16:33:38 -0300

    feat: delete localStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -5,9 +5,20 @@ const useLocalStorage = () => {
   const readLocalStorage = (key: string) => JSON.parse(
     localStorage.getItem(key) as string,
   );
+  const deleteItemLocalStorage = (key: string, id: string) => {
+    const data = readLocalStorage(key);
+    const newData = data.filter((item: any) => item.id !== id);
+    localStorage.removeItem(key);
+    saveLocalStorage(key, newData);
+  };
   const deleteLocalStorage = (key: string) => {
     localStorage.removeItem(key);
   };
-  return { saveLocalStorage, readLocalStorage, deleteLocalStorage };
+  return {
+    saveLocalStorage,
+    readLocalStorage,
+    deleteLocalStorage,
+    deleteItemLocalStorage,
+  };
 };
 export default useLocalStorage;
